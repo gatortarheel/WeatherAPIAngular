934fd60f07dc8873d96142ee51b781ab1f1a7805
Russell Smith <russell@taradel.com>
2018-01-26 07:46:10 -0500

    Add project files.

---
+++ AngularJS2018/Scripts/root.js
@@ -0,0 +1,15 @@
+﻿root = angular.module('root', []);
+
+root.controller("index", ["$scope", function ($scope) {
+        $scope.message = "Hello World!";
+        $scope.potato = "hot potato";
+        $scope.favoriteWord;
+        $scope.favoriteColor;
+        $scope.favoriteShape;
+        $scope.customersToContact;
+    }]);
+
+root.controller("index2", ["$scope", function ($scope) {
+    $scope.message = "Hello World!2";
+    $scope.potato = "hot potato2";
+}]);
